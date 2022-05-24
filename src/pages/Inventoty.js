79ab0d0f924e2b9e04af8818a55b9a88829bd8dc
Toever1503 <Haunv1503@gmail.com>
2022-05-24 23:26:46 +0700

    Chia lại component quản lý khó
    Chia component  quản lý khoa ra 2 component export và import

--- src/pages/Inventoty.js
+++ src/pages/Inventoty.js
@@ -1,207 +1,25 @@
-import { Tabs, Button, Tooltip, Segmented, Table, Modal, Select, Spin } from 'antd';
-import { DownloadOutlined } from '@ant-design/icons';
+import { Tabs } from 'antd';
 import './css/inventory.css';
-import { useState } from 'react';
+import ImportProducts from './components/ImportProducts';
+import ExportProducts from './components/ExportProducts';
 
 
-const { Option } = Select;
 const { TabPane } = Tabs;
 function changeTab(key) {
     console.log(key);
 }
-const columns = [
-    {
-        title: 'ID',
-        dataIndex: 'id',
-        key: '1',
-        width: 150,
-    },
-    {
-        title: 'Người Nhập',
-        dataIndex: 'user',
-        key: '2',
-        width: 150,
-    },
-    {
-        title: 'Ghi chú',
-        dataIndex: 'note',
-        key: '3',
-        width: 150,
-    },
-    {
-        title: 'Tổng Tiền',
-        dataIndex: 'totalMoney',
-        key: '4',
-        width: 150,
-    },
-    {
-        title: 'Tổng Số Lượng',
-        dataIndex: 'totalQuantity',
-        key: '4a1',
-        width: 150,
-    },
-    {
-        title: 'Còn Trong Kho',
-        dataIndex: 'totalRemain',
-        key: '42',
-        width: 150,
-    },
-    {
-        title: 'Ngày Tạo',
-        dataIndex: 'createdDate',
-        key: '5',
-        width: 150,
-    },
-    {
-        title: 'Cập Nhật Cuối',
-        dataIndex: 'updatedDate',
-        key: '53',
-        width: 150,
-    },
-    {
-        title: 'Trạng Thái',
-        key: 'status',
-        fixed: 'right',
-        width: 70,
-        render: (data) => <Tooltip title="" color='cyan' key='red'>
-            <Button type="danger" size='small' >
-                {data.status}
-            </Button>
-        </Tooltip>,
-    },
-];
-
-const handlePageTableChange = (pagination) => {
-    console.log(pagination);
-};
-
 
 export default function Inventory() {
-    const [data, setData] = useState([
-        {
-            id: '1',
-            user: 'Nguyễn Văn A',
-            note: 'Nhập từ kho',
-            totalMoney: '100.000$',
-            createdDate: '2020-01-01',
-            status: 'Hủy',
-        },
-        {
-            id: '2',
-            user: 'Nguyễn Văn B',
-            note: 'Nhập từ kho',
-            totalMoney: '100.000$',
-            createdDate: '2020-01-01',
-            status: 'Hủy',
-        }
-    ]);
-    const [loading, setLoading] = useState(false);
-    const [page, setPage] = useState({
-        current: 1,
-        pageSize: 10,
-        total: 1000,
-    });
-    const [isModalVisible, setIsModalVisible] = useState(false);
-
-    const showModal = (record) => {
-        setIsModalVisible(true);
-        console.log('show modal')
-        console.log('record', record);
-    };
-
-    const handleOk = () => {
-        setIsModalVisible(false);
-    };
-
-    const handleCancel = () => {
-        setIsModalVisible(false);
-    };
-
-    const modalolumns = [
-        {
-            title: 'STT',
-            dataIndex: 'id',
-            key: '1',
-            width: 150,
-        },
-        {
-            title: 'Sản Phẩm',
-            dataIndex: 'product',
-            key: 'product',
-            width: 150,
-        },
-        {
-            title: 'Số Lượng',
-            dataIndex: 'quantity',
-            key: 'quantity',
-            width: 150,
-        },
-        {
-            title: 'Đơn Giá',
-            dataIndex: 'price',
-            key: 'price',
-            width: 150,
-        },
-    ];
-    const handleChange = selectedItems => {
-    };
-    const handleSelect = (e)=>{
-        console.log(e);
-    }
-    
     return (
         <>
-
             <div id='inventory-management'>
-                <Modal title="Thông Tin Chi tiết" zIndex='1500' width='80%' visible={isModalVisible} 
-                okText="Lưu"
-                onOk={handleOk}
-                cancelText="Đã Xác Nhận"
-                onCancel={handleCancel}>
-                    <Select
-                        showSearch
-                        style={{ width: 200 }}
-                        placeholder="Nhập tên, mã sản phẩm"
-                        dropdownStyle={{ zIndex: 1600 }} onChange={handleSelect}  >
-                        <Option value="1">Not Identified</Option>
-                        <Option value="2">Closed</Option>
-                        <Option value="3">Communicated</Option>
-                        <Option value="4">Identified</Option>
-                        <Option value="5">Resolved</Option>
-                        <Option value="6">Cancelled</Option>
-                    </Select>
-                    <Table columns={modalolumns} />
-                </Modal>
                 <Tabs defaultActiveKey="import" onChange={changeTab}>
                     <TabPane tab="Nhập kho" key="import" >
-                        <div className='page-header-actions'>
-                            <Tooltip title="prompt text" color='cyan' key='red'>
-                                <Button type="primary" icon={<DownloadOutlined />} size='small' />
-                            </Tooltip>
-                            <Tooltip title="prompt text" color='cyan' key='red'>
-                                <Button type="primary" icon={<DownloadOutlined />} size='small' />
-                            </Tooltip>
-                            <Tooltip title="prompt text" color='cyan' key='red'>
-                                <Button type="primary" icon={<DownloadOutlined />} size='small' />
-                            </Tooltip>
-                            <Tooltip title="prompt text" color='cyan' key='red'>
-                                <Button type="primary" icon={<DownloadOutlined />} size='small' />
-                            </Tooltip>
-                        </div>
-                        <Segmented options={['Tất cả', 'Weekly', 'Monthly', 'Quarterly', 'Yearly']} />
-                        <Table onRow={(record, rowIndex) => {
-                            return {
-                                onClick: event => showModal(record), // click row
-                            };
-                        }} pagination={page} loading={loading} onChange={handlePageTableChange}
-                            columns={columns} dataSource={data} scroll={{ x: 1500, y: 300 }} />
-
-
+                        <ImportProducts />
                     </TabPane>
                     <TabPane tab="Xuất kho" key="export">
-                        b
+                        <ExportProducts />
                     </TabPane>
-
                 </Tabs>
             </div>
 
---
+++ src/pages/components/ExportProducts.js
@@ -0,0 +1,204 @@
+import { Button, Tooltip, Segmented, Table, Modal, Select } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+
+const { Option } = Select;
+export default function ExportProducts() {
+
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: '1',
+            width: 150,
+        },
+        {
+            title: 'Người Xuất',
+            dataIndex: 'user',
+            key: '2',
+            width: 150,
+        },
+        {
+            title: 'Ghi chú',
+            dataIndex: 'note',
+            key: '3',
+            width: 150,
+        },
+        {
+            title: 'Tổng Tiền',
+            dataIndex: 'totalMoney',
+            key: '4',
+            width: 150,
+        },
+        {
+            title: 'Tổng Số Lượng',
+            dataIndex: 'totalQuantity',
+            key: '4a1',
+            width: 150,
+        },
+        {
+            title: 'Còn Trong Kho',
+            dataIndex: 'totalRemain',
+            key: '42',
+            width: 150,
+        },
+        {
+            title: 'Ngày Tạo',
+            dataIndex: 'createdDate',
+            key: '5',
+            width: 150,
+        },
+        {
+            title: 'Cập Nhật Cuối',
+            dataIndex: 'updatedDate',
+            key: '53',
+            width: 150,
+        },
+        {
+            title: 'Trạng Thái',
+            key: 'status',
+            fixed: 'right',
+            width: 70,
+            render: (data) => <Tooltip title="" color='cyan' key='red'>
+                <Button type="danger" size='small' >
+                    {data.status}
+                </Button>
+            </Tooltip>,
+        },
+    ];
+    const [data, setData] = useState([
+        {
+            id: '1',
+            user: 'Nguyễn Văn A',
+            note: 'Nhập từ kho',
+            totalMoney: '100.000$',
+            createdDate: '2020-01-01',
+            status: 'Hủy',
+        },
+        {
+            id: '2',
+            user: 'Nguyễn Văn B',
+            note: 'Nhập từ kho',
+            totalMoney: '100.000$',
+            createdDate: '2020-01-01',
+            status: 'Hủy',
+        }
+    ]);
+    const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState({
+        current: 1,
+        pageSize: 10,
+        total: 1000,
+    });
+
+    const handlePageTableChange = (pagination) => {
+        console.log(pagination);
+    };
+
+    const modalolumns = [
+        {
+            title: 'STT',
+            dataIndex: 'id',
+            key: '1',
+            width: 150,
+        },
+        {
+            title: 'Sản Phẩm',
+            dataIndex: 'product',
+            key: 'product',
+            width: 150,
+        },
+        {
+            title: 'Số Lượng',
+            dataIndex: 'quantity',
+            key: 'quantity',
+            width: 150,
+        },
+        {
+            title: 'Đơn Giá',
+            dataIndex: 'price',
+            key: 'price',
+            width: 150,
+        },
+    ];
+    const [modalOptions, setModalOptions] = useState([
+        {
+            id: 1,
+            product: 'Sản Phẩm 1',
+        },
+        {
+            id: 2,
+            product: 'Sản Phẩm 2',
+        },
+        {
+            id: 3,
+            product: 'Sản Phẩm 3',
+        },
+    ]);
+    const handleSelectChange = (e) => {
+        console.log(e);
+        setModalOptions([]);
+    }
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = (record) => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        // setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        // setIsModalVisible(false);
+    };
+
+
+    return (
+        <>
+            <Modal title="Thông Tin Chi tiết" zIndex='1500' width='80%' visible={isModalVisible}
+                okText="Lưu"
+                onOk={handleOk}
+                cancelText="Đã Xác Nhận"
+                onCancel={handleCancel}>
+                <Select
+                    showSearch
+                    style={{ width: 500 }}
+                    placeholder="Nhập tên, mã sản phẩm"
+                    dropdownStyle={{ zIndex: 1600 }} onChange={handleSelectChange}  >
+                    {
+                        modalOptions.length !== 0 && modalOptions.map(item => (
+                            <Option key={item.id} value={item.id} style={{ display: 'flex' }}>
+                                <p style={{ width: '30%', display: 'inline-block', padding: '0' }}>Mã: {item.id}</p>
+                                <p style={{ width: '50%', display: 'inline-block', padding: '0' }}>Tên: {item.product}</p>
+                            </Option>
+                        ))
+                    }
+                </Select>
+                <Table columns={modalolumns} />
+            </Modal>
+            <div className='page-header-actions'>
+                <Tooltip title="prompt text" color='cyan' key='red'>
+                    <Button type="primary" icon={<DownloadOutlined />} size='small' />
+                </Tooltip>
+                <Tooltip title="prompt text" color='cyan' key='red1'>
+                    <Button type="primary" icon={<DownloadOutlined />} size='small' />
+                </Tooltip>
+                <Tooltip title="prompt text" color='cyan' key='red2'>
+                    <Button type="primary" icon={<DownloadOutlined />} size='small' />
+                </Tooltip>
+                <Tooltip title="prompt text" color='cyan' key='red23'>
+                    <Button type="primary" icon={<DownloadOutlined />} size='small' />
+                </Tooltip>
+            </div>
+            <Segmented options={['Tất cả', 'Weekly', 'Monthly', 'Quarterly', 'Yearly']} />
+            <Table onRow={(record, rowIndex) => {
+                return {
+                    onClick: event => showModal(record), // click row
+                };
+            }} pagination={page} loading={loading} onChange={handlePageTableChange}
+                columns={columns} dataSource={data} scroll={{ x: 1500, y: 300 }} />
+
+        </>
+    );
+}
